237cde20134429223d721e953059bcf8e87a8fc7
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-11 17:37:58 +0900

    payjpを実装するためのJavaScriptの準備。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,11 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+// application.jsから見てcard.jsは1つ上の階層に位置しているため
+require("../card")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
