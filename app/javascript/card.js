91e426eb05b961df3d4d401d229bfb1cb71be6d1
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-11 18:49:14 +0900

    card.jsの編集。
    クレジットカードに関する情報をPAYJPに送信し、PAYJPから受けたトークンをコントローラーへ送信する。（コントローラーに送る際に、クレジットカード情報が含まれないよう削除。）

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,6 +2,35 @@ const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // 環境変数を読み込む
   const form = document.getElementById("charge-form"); 
   form.addEventListener("submit", (e) => { // イベント発火
-    e.preventDefault();
-  });
- };
+  e.preventDefault();
+    // カード情報の取得先
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = { // カードオブジェクトを生成
+        number: formData.get("number"),              // カード番号
+        cvc: formData.get("cvc"),                    // カード裏面の3桁の数字
+        exp_month: formData.get("exp_month"),        // 有効期限の月
+        exp_year: `20${formData.get("exp_year")}`,   // 有効期限の年
+      };
+      // payjpに送信する
+      Payjp.createToken(card, (status, response) => {
+        if (status === 200) {
+          // PAY.JPから返ってきたパラメーターresponseから、トークンに該当する値のみを抽出し、変数tokenに代入
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");   //idを元に要素を取得
+          const tokenObj = `<input value=${token} name='card_token' type="hidden">`;   //paramsの中にトークンを含める
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);  //フォームの内部の最後の子要素の後に挿入(charge-form内の子要素の最後)
+
+          document.getElementById("number").removeAttribute("name");
+          document.getElementById("cvc").removeAttribute("name");
+          document.getElementById("exp_month").removeAttribute("name");
+          document.getElementById("exp_year").removeAttribute("name");
+
+          document.getElementById("charge-form").submit();
+        }
+      });
+    });
+  };
+
+ window.addEventListener("load", pay);
